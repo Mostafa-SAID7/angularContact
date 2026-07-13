d7be596b9fb398974c54483842917aacca29a451
Mostafa-SAID7 <m.ssaid356@gmail.com>
2026-07-13 22:59:32 +0300

    chore: remove VS artifacts, update packages, clean tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', 
-    content: [
-      "./Pages/**/*.{cshtml,razor,html}",
-      "./Views/**/*.{cshtml,razor,html}",
-      "./wwwroot/**/*.html",
-      "./**/*.cshtml",
-      "./src/**/*.{html,ts}"
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
   
